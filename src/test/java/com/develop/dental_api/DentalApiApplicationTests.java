99c74ba816fa7a17cb6856b80635263076cc1300
jsalinas4 <jsalinasr3@upao.edu.pe>
2025-04-28 23:00:47 -0500

    v0.0.1
    Spring

---
+++ src/test/java/com/develop/dental_api/DentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.dental_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
